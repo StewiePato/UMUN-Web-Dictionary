77327654204f3b86ba5131c84f8cd4a91c9d8795
Carlos <carlosoltralozano@gmail.com>
2024-02-18 14:22:56 +0100

    Mejor busqueda

--- main.js
+++ main.js
@@ -22,8 +22,20 @@ function parseDoc(csvContent){
     });
     rows.shift()
     rows.sort((a, b) => a[0].localeCompare(b[0]))
-    global_list = rows
-    drawRows(rows)
+    for(row of rows){
+        if(row[0] != ''){
+            let word = capitalizeFirstLetter(row[0])
+            let type = row[1]
+            let def = capitalizeFirstLetter(row[2])
+            let translations = ''
+            row[3].split("/").flat().forEach(element => {
+                translations += `<span>${capitalizeFirstLetter(element)}</span>/`
+            });
+            translations = translations.substring(0, translations.length-1)
+            global_list.push([word, type, def, translations])
+        }
+    }
+    drawRows(global_list)
 }
 
 function capitalizeFirstLetter(string) {
@@ -33,26 +45,20 @@ function capitalizeFirstLetter(string) {
 function drawRows(rows){
     let lastLetter = ''
     for(row of rows){
-        if(row[0] != ''){
-            if(row[0].charAt(0).toUpperCase() != lastLetter){
-                lastLetter = row[0].charAt(0).toUpperCase()
-                let container = `<div class="container" id="${lastLetter}_container"></div>`
-                $("body").append(container)
-                $(`#${lastLetter}_container`).append(`<h2 id="${lastLetter}">${lastLetter}</h2>`)
-            }
-            let word = capitalizeFirstLetter(row[0])
-            let type = row[1]
-            let def = capitalizeFirstLetter(row[2])
-            let trans = capitalizeFirstLetter(row[3])
-            let result = 
-                `<div class="entry">`+
-                    `<h3>${word}</h3>`+
-                    `<small>Tipo: ${type}</small>`+
-                    `<p>${def}</p>`+
-                    `<p class="translation">Traducción más cercana: ${trans}</p>`+
-                `</div>`
-            $(`#${lastLetter}_container`).append(result)
+        if(row[0].charAt(0).toUpperCase() != lastLetter){
+            lastLetter = row[0].charAt(0).toUpperCase()
+            let container = `<div class="container" id="${lastLetter}_container"></div>`
+            $("body").append(container)
+            $(`#${lastLetter}_container`).append(`<h2 id="${lastLetter}">${lastLetter}</h2>`)
         }
+        let result = 
+            `<div class="entry">`+
+                `<h3><span>${row[0]}</span></h3>`+
+                `<small>Tipo: ${row[1]}</small>`+
+                `<p>${row[2]}</p>`+
+                `<p class="translation">Traducción más cercana: ${row[3]}</p>`+
+            `</div>`
+        $(`#${lastLetter}_container`).append(result)
     }
 }
 
@@ -60,5 +66,10 @@ function search(goal){
     $(".container").each(function(index, element) {
         $(element).remove();
     });
-    drawRows(global_list.filter((row) => row[0].toUpperCase().startsWith(goal.toUpperCase())))
+    drawRows(global_list.filter((row) => row[0].toUpperCase().startsWith(goal.toUpperCase()) || row[3].split("/").some(function(e){return e.toUpperCase().startsWith(("<span>" + goal).toUpperCase())})))
+    $("span").each(function(index, element) {
+        if($(element).html().toUpperCase().startsWith(goal.toUpperCase()) && goal != ''){
+            $(element).addClass("highlighted")
+        }
+    });
 }
