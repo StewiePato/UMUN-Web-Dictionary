41a5761531ae58551e637a204db2050667045dae
coltralo <coltralo@emeal.nttdata.coM>
2024-03-14 09:58:21 +0100

    Prueba cors

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const SHEET_URL = 'https://docs.google.com/spreadsheets/d/1hv-NzuCEbHF0H5mnTHFOCmqKLVvrwhBaW8hJ419L8Hw/gviz/tq?tqx=out:csv&sheeh2t=Listado'
-const PROXY_URL = 'https://corsproxy.io/?'
+const PROXY_URL = 'https://proxy.cors.sh/'
 let global_list = []
 $(document).ready(function(){
     document.getElementById("autoFocusSearchCB").checked = localStorage.getItem('autoFocusSearch') == 'true'
